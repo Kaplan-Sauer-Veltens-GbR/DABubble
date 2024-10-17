1bcf19d8d914437587290203e7de9071efe1356a
Phillipcodes <Phillipsauer00@gmail.com>
2024-10-17 20:12:02 +0200

    text message field style mainly completed

--- src/app/playground-phillip/playground-phillip.component.ts
+++ src/app/playground-phillip/playground-phillip.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IconLibaryComponent } from '../shared/components/icon-component/icon-libary.component';
 import { InputFieldsComponent } from '../shared/components/inputs/input-fields/input-fields.component';
+import { TextMessageFieldComponent } from '../shared/components/inputs/text-message-field/text-message-field.component';
 
 @Component({
   selector: 'app-playground-phillip',
   standalone: true,
-  imports: [CommonModule,IconLibaryComponent,InputFieldsComponent],
+  imports: [CommonModule,IconLibaryComponent,InputFieldsComponent,TextMessageFieldComponent],
   templateUrl: './playground-phillip.component.html',
   styleUrl: './playground-phillip.component.scss'
 })
--- src/app/shared/components/inputs/text-message-field/text-message-field.component.ts
+++ src/app/shared/components/inputs/text-message-field/text-message-field.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IconLibaryComponent } from "../../icon-component/icon-libary.component";
 
 @Component({
   selector: 'app-text-message-field',
   standalone: true,
-  imports: [],
+  imports: [IconLibaryComponent],
   templateUrl: './text-message-field.component.html',
   styleUrl: './text-message-field.component.scss'
 })
 export class TextMessageFieldComponent {
-
+  @Input() placeholder:string = 'Enter';
+  @Input() pattern:string = '';
+  @Input() required: boolean = false
 }
