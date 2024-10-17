b75ea4ae0ed58f5078bb15539246a52fc86ac08c
Phillipcodes <Phillipsauer00@gmail.com>
2024-10-17 21:38:08 +0200

    deleted fragments of phillips playground

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { ChatMessageComponent } from './shared/components/chat/chat-message/chat-message.component';
-import { PlaygroundPhillipComponent } from './playground-phillip/playground-phillip.component';
+
 
 export const routes: Routes = [
     {path: 'playground', component: ChatMessageComponent},
-    {path: 'playgroundPhil', component:PlaygroundPhillipComponent}
+
 ];
