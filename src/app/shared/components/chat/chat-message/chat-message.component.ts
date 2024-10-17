93c31a9ae880cf646eaff7407e9e772c1887354f
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-10-17 21:40:27 +0200

    basic layout of chat-msgs

--- src/app/shared/components/chat/chat-message/chat-message.component.ts
+++ src/app/shared/components/chat/chat-message/chat-message.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-chat-message',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './chat-message.component.html',
   styleUrl: './chat-message.component.scss'
 })
 export class ChatMessageComponent {
-
+  @Input() isAuthor: boolean = false;
+  avatarPath: string = '/assets/images/avatars/demo_avatar.png';
 }
