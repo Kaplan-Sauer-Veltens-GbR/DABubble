970296a3a29a9d28f0c2b0cbf3672f16b04aee4b
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-10-17 14:37:47 +0200

    cleaned up app.component. prepared unit testing. created a transloco-testing.module for easier testing in every spec.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,14 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslocoModule } from '@jsverse/transloco';
-import { RadioButtonComponent } from "./shared/components/inputs/radio-button/radio-button.component";
-import { WorkspaceFabComponent } from "./main/workspace-menu/workspace-fab/workspace-fab.component";
-import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TranslocoModule, RadioButtonComponent, WorkspaceFabComponent],
+  imports: [RouterOutlet, TranslocoModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  auth: AuthService = inject(AuthService);
-
-  ngOnInit(): void {
- 
-    
-  }
   
 }
