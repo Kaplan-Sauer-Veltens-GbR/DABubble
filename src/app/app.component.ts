715bbd75acf9cad5ee5f5a0413724c49fa60b110
Phillipcodes <Phillipsauer00@gmail.com>
2024-10-17 15:34:10 +0200

    Input-fields
    Basic styling of input fields and an if statement to check if it is an icon-based input field.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { TranslocoModule } from '@jsverse/transloco';
 import { RadioButtonComponent } from "./shared/components/inputs/radio-button/radio-button.component";
 import { WorkspaceFabComponent } from "./main/workspace-menu/workspace-fab/workspace-fab.component";
 import { AuthService } from './services/auth.service';
+import { InputFieldsComponent } from './shared/components/inputs/input-fields/input-fields.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TranslocoModule, RadioButtonComponent, WorkspaceFabComponent],
+  imports: [RouterOutlet, TranslocoModule, RadioButtonComponent, WorkspaceFabComponent,InputFieldsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
