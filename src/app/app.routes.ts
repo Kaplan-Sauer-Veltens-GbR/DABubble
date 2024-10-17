8c380217c6ffc76c3c5a3e99c1c38c01d4da237e
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-10-17 15:03:49 +0200

    further adjustments to get a clean testing environment. please test your components under path playground. just change the component to the feature-component you are need to test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ChatMessageComponent } from './shared/components/chat/chat-message/chat-message.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'playground', component: ChatMessageComponent}
+];
