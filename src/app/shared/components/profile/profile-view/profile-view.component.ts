1dbb39fd4464d6a76e19f460c212a551040e3883
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-10-18 18:06:54 +0200

    implemented versatile button-component

--- src/app/shared/components/profile/profile-view/profile-view.component.ts
+++ src/app/shared/components/profile/profile-view/profile-view.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { IconLibaryComponent } from "../../icon-component/icon-libary.component";
 import { TranslocoModule } from '@jsverse/transloco';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from "../../inputs/button/button.component";
 
 @Component({
   selector: 'app-profile-view',
   standalone: true,
-  imports: [IconLibaryComponent, TranslocoModule, CommonModule],
+  imports: [IconLibaryComponent, TranslocoModule, CommonModule, ButtonComponent],
   templateUrl: './profile-view.component.html',
   styleUrl: './profile-view.component.scss'
 })
