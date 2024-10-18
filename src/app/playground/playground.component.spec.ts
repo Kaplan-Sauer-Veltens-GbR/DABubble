a4ea81c1ab5fcc693337bcfdf4bedbc6a165c985
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-10-18 14:38:45 +0200

    minor adjustment in playground-component-spec

--- src/app/playground/playground.component.spec.ts
+++ src/app/playground/playground.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PlaygroundComponent } from './playground.component';
+import { getTranslocoModule } from '../modules/transloco-testing/transloco-testing.module';
 
 describe('PlaygroundComponent', () => {
   let component: PlaygroundComponent;
@@ -8,7 +9,9 @@ describe('PlaygroundComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PlaygroundComponent]
+      imports: [
+        getTranslocoModule(),
+        PlaygroundComponent]
     })
     .compileComponents();
 
