01b7e2d879b45e1b47be2d258ef07f81fe39a927
Phillip <160394082+Phillipcodes@users.noreply.github.com>
2024-10-19 16:16:23 +0200

    added other component dependecies

--- src/app/chat/channel-edit-popup/channel-edit-popup.component.ts
+++ src/app/chat/channel-edit-popup/channel-edit-popup.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { InputFieldsComponent } from "../../shared/components/inputs/input-fields/input-fields.component";
+import { IconLibaryComponent } from "../../shared/components/icon-component/icon-libary.component";
 
 @Component({
   selector: 'app-channel-edit-popup',
   standalone: true,
-  imports: [],
+  imports: [InputFieldsComponent, IconLibaryComponent],
   templateUrl: './channel-edit-popup.component.html',
   styleUrl: './channel-edit-popup.component.scss'
 })
