ef64c2e344a3ae645d31185006cf417d0ccb77bb
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-10-20 14:28:40 +0200

    restructured profile-view to prepare profile-edit

--- src/app/shared/components/profile/profile.component.ts
+++ src/app/shared/components/profile/profile.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { ProfileViewComponent } from "./profile-view/profile-view.component";
 import { ProfileEditComponent } from './profile-edit/profile-edit.component';
+import { IconLibaryComponent } from '../icon-component/icon-libary.component';
+import { CommonModule } from '@angular/common';
+import { TranslocoModule } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [ProfileViewComponent, ProfileEditComponent],
+  imports: [CommonModule, ProfileViewComponent, ProfileEditComponent, IconLibaryComponent, TranslocoModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
-  @Input() view: 'regular' | 'edit' = 'edit';
+  @Input() view: 'regular' | 'edit' = 'regular';
   @Input() user = {
     name: 'Paulchen Peterson',
     isOnline: true
