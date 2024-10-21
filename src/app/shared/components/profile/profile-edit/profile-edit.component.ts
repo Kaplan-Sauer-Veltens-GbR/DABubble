d1b0440a5bbf3201fc4424221af64eee2d7add44
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-10-21 13:18:09 +0200

    added edit-view-draft

--- src/app/shared/components/profile/profile-edit/profile-edit.component.ts
+++ src/app/shared/components/profile/profile-edit/profile-edit.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TranslocoModule } from '@jsverse/transloco';
+import { IconLibaryComponent } from '../../icon-component/icon-libary.component';
+import { InputFieldsComponent } from "../../inputs/input-fields/input-fields.component";
+import { ButtonComponent } from "../../inputs/button/button.component";
 
 @Component({
   selector: 'app-profile-edit',
   standalone: true,
-  imports: [],
+  imports: [TranslocoModule, IconLibaryComponent, InputFieldsComponent, ButtonComponent],
   templateUrl: './profile-edit.component.html',
   styleUrl: './profile-edit.component.scss'
 })
 export class ProfileEditComponent {
-
+  @Input() user = {
+    name: 'Paulchen Peterson',
+    isOnline: true
+  }
 }
