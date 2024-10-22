6ff7d3d3117c04ea1058ffefea51250710d1d190
Phillipcodes <Phillipsauer00@gmail.com>
2024-10-22 17:01:05 +0200

    set up for avatar tag line component

---
+++ src/app/shared/components/user-avatar-tag/user-avatar-tag.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user-avatar-tag',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-avatar-tag.component.html',
+  styleUrl: './user-avatar-tag.component.scss'
+})
+export class UserAvatarTagComponent {
+@Input() src:string = './assets/images/avatars/demo_avatar.png'
+@Input() isYou:boolean = false;
+}
