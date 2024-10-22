0c8b65a120a8c94f4f93810b1c619591d5f4de5f
Phillipcodes <Phillipsauer00@gmail.com>
2024-10-22 18:33:11 +0200

    finished styling for user avtar tag

--- src/app/shared/components/user-avatar-tag/user-avatar-tag.component.ts
+++ src/app/shared/components/user-avatar-tag/user-avatar-tag.component.ts
@@ -1,13 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-user-avatar-tag',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-avatar-tag.component.html',
   styleUrl: './user-avatar-tag.component.scss'
 })
 export class UserAvatarTagComponent {
+ 
 @Input() src:string = './assets/images/avatars/demo_avatar.png'
 @Input() isYou:boolean = false;
+
+
 }
