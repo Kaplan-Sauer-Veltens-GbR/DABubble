dd17d53008d60de1b6161039893e44f0ba5de6b1
Phillipcodes <Phillipsauer00@gmail.com>
2024-10-22 13:36:01 +0200

    tested if the if else corrcetly change the html based on the 2nd span

--- src/app/chat/pop-ups/channel-edit-popup/channel-edit-popup.component.spec.ts
+++ src/app/chat/pop-ups/channel-edit-popup/channel-edit-popup.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChannelEditPopupComponent } from './channel-edit-popup.component';
+import { By } from '@angular/platform-browser';
 
 describe('ChannelEditPopupComponent', () => {
   let component: ChannelEditPopupComponent;
@@ -20,4 +21,25 @@ describe('ChannelEditPopupComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should select the second span element editMode=false', () => {
+    component.editMode = false;
+    fixture.detectChanges();
+    const spanElements = fixture.debugElement.queryAll(By.css('span'));  
+    const secondSpan = spanElements[1].nativeElement; 
+
+    
+    expect(secondSpan.textContent).toBe('Bearbeiten');  
+  });
+  it('should select the second span element editMode=true', () => {
+    component.editMode = true;
+    fixture.detectChanges();
+    const spanElements = fixture.debugElement.queryAll(By.css('span'));
+    const secondSpan = spanElements[1].nativeElement; 
+
+    
+    expect(secondSpan.textContent).toBe('Speichern');  
+  });
+  
 });
+
+
