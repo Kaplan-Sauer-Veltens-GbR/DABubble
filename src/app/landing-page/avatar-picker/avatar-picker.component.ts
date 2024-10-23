7de927be46a7acaca66b133a720d99ca10f5f1bd
Okan Kaplan <okan149@gmail.com>
2024-10-23 18:14:12 +0200

    styling avatar picker component. adding header + card + unset avatar

--- src/app/landing-page/avatar-picker/avatar-picker.component.ts
+++ src/app/landing-page/avatar-picker/avatar-picker.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { IconLibaryComponent } from '../../shared/components/icon-component/icon-libary.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonComponent } from '../../shared/components/inputs/button/button.component';
+import { InputFieldsComponent } from '../../shared/components/inputs/input-fields/input-fields.component';
 
 @Component({
   selector: 'app-avatar-picker',
   standalone: true,
-  imports: [],
+  imports: [IconLibaryComponent,CommonModule,ButtonComponent],
   templateUrl: './avatar-picker.component.html',
   styleUrl: './avatar-picker.component.scss'
 })
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SignInComponent } from "./sign-in/sign-in.component";
 import { SignUpComponent } from "./sign-up/sign-up.component";
+import { AvatarPickerComponent } from "./avatar-picker/avatar-picker.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [SignInComponent, SignUpComponent],
+  imports: [SignInComponent, SignUpComponent, AvatarPickerComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
