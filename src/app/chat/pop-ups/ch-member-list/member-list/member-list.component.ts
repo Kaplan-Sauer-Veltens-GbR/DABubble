bc796d9d28c2de0b2f51266da0a1babadd75304d
Phillip <160394082+Phillipcodes@users.noreply.github.com>
2024-10-23 12:29:16 +0200

    styling for the member list

--- src/app/chat/pop-ups/ch-member-list/member-list/member-list.component.ts
+++ src/app/chat/pop-ups/ch-member-list/member-list/member-list.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { AvatarBarComponent } from "../../../../shared/components/chat/avatar-bar/avatar-bar.component";
+import { UserAvatarTagComponent } from "../../../../shared/components/user-avatar-tag/user-avatar-tag.component";
+import { ReactionPopoverComponent } from "../../../../shared/components/chat/chat-message/reaction-popover/reaction-popover.component";
+import { IconLibaryComponent } from "../../../../shared/components/icon-component/icon-libary.component";
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [],
+  imports: [AvatarBarComponent, UserAvatarTagComponent, ReactionPopoverComponent, IconLibaryComponent],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.scss'
 })
