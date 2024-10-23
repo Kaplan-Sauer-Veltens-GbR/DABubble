44f0390abffb32840855366ac28014bf8d70ede5
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-10-23 14:00:17 +0200

    first version of reaction-translation

--- src/app/shared/components/chat/chat-message/reaction-bar/single-reaction-icon/reaction-details/reaction-details.component.ts
+++ src/app/shared/components/chat/chat-message/reaction-bar/single-reaction-icon/reaction-details/reaction-details.component.ts
@@ -3,6 +3,7 @@ import { IconName } from '../../../../../../../interfaces/icon-names.model';
 import { IconLibaryComponent } from '../../../../../icon-component/icon-libary.component';
 import { TranslocoModule } from '@jsverse/transloco';
 import { WordlistService } from '../../../../../../../services/wordlist.service';
+import { capitalize } from '../../../../../../../utils/string.utils';
 
 @Component({
   selector: 'app-reaction-details',
@@ -12,15 +13,18 @@ import { WordlistService } from '../../../../../../../services/wordlist.service'
   styleUrl: './reaction-details.component.scss'
 })
 export class ReactionDetailsComponent {
+
+  //CAVE: Make sure in production user[index] is not the index of current user
+
   wordlist: WordlistService = inject(WordlistService);
 
   @Input() reaction!: IconName;
-  exampleUsers: string[] = ['Kevin', 'Torben'];
-  selfReacted: boolean = true;
+  exampleUsers: string[] = ['Kevin', 'Torben', 'Paul'];
+  selfReacted: boolean = false;
 
   firstReaction() {
     return this.exampleUsers.length == 1 && this.selfReacted ?
-      this.wordlist.translate('you') :
+      capitalize(this.wordlist.translate('you')) :
       this.exampleUsers[0];
   }
 
@@ -28,30 +32,21 @@ export class ReactionDetailsComponent {
     if (onlyOneReaction) {
       return this.selfReacted ? 'reaction-summary.only-one.own' : 'reaction-summary.only-one.other';
     } else {
-      return 'reaction-summary.multiple' //reacted : have reacted
+      return 'reaction-summary.multiple';
     }
   }
-
-  getGrammarArgument() {
-    if (!this.selfReacted && this.exampleUsers.length == 2) {
-      return { 'argument': 'have ' };
-    } else {
-      return { 'argument': '' };
-    }
-  }
-
 
   otherReactions() {
     let total = this.exampleUsers.length - 1;
     if (total == 1) {
       return this.selfReacted ?
-        this.wordlist.translate('and', 'you') :
-        `${this.wordlist.translate('and')} ${this.exampleUsers[1]}`;
+        ` ${this.wordlist.translate('and', 'you')}` :
+        ` ${this.wordlist.translate('and')} ${this.exampleUsers[1]}`;
     } else if (total >= 2) {
       if (this.selfReacted) {
-        return total - 1 == 1 ? `ein anderer und Du` : `, ${total - 1} andere und Du`;
+        return total - 1 == 1 ? `, ${this.exampleUsers[1]} ${this.wordlist.translate('and', 'you')}` : `, ${total - 1} ${this.wordlist.translate('others', 'and', 'you')}`;
       } else {
-        return `und ${total} andere`;
+        return ` ${this.wordlist.translate('and')} ${total} ${this.wordlist.translate('others')}`;
       }
     }
     return '';
