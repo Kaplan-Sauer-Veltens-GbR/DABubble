3ee3deb014e8271fd5f68a9f25571c1e0024f028
Phillipcodes <Phillipsauer00@gmail.com>
2024-10-25 00:20:53 +0200

    removed iconlibarycomponent import form inputfields

--- src/app/shared/components/inputs/input-fields/input-fields.component.ts
+++ src/app/shared/components/inputs/input-fields/input-fields.component.ts
@@ -5,7 +5,7 @@ import { IconLibaryComponent } from '../../icon-component/icon-libary.component'
 @Component({
   selector: 'app-input-fields',
   standalone: true,
-  imports: [CommonModule,IconLibaryComponent],
+  imports: [CommonModule],
   templateUrl: './input-fields.component.html',
   styleUrl: './input-fields.component.scss'
 })
