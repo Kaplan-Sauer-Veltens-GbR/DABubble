8030f92ba00b901a24bd86ddb26a53e5f12fcd2a
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-10-24 14:43:56 +0200

    first draft of avatar-menu

--- src/app/main/header/avatar-menu/avatar-menu.component.ts
+++ src/app/main/header/avatar-menu/avatar-menu.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { IconLibaryComponent } from "../../../shared/components/icon-component/icon-libary.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'avatar-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, IconLibaryComponent],
   templateUrl: './avatar-menu.component.html',
   styleUrl: './avatar-menu.component.scss'
 })
 export class AvatarMenuComponent {
+  exampleUser: {name: string, onlineStatus: 'online' | 'away' | 'offline'} = {
+    name: 'Frederik Beck',
+    onlineStatus: 'offline'
+  }
 
 }
