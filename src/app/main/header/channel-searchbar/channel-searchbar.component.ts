4db6b18dcdf2cb6056d2f5d2bc5663bbc7537dea
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-10-25 11:02:07 +0200

    fixed accessibility issues related to user-avatars

--- src/app/main/header/channel-searchbar/channel-searchbar.component.ts
+++ src/app/main/header/channel-searchbar/channel-searchbar.component.ts
@@ -1,10 +1,11 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { IconLibaryComponent } from '../../../shared/components/icon-component/icon-libary.component';
+import { TranslocoModule } from '@jsverse/transloco';
 
 @Component({
   selector: 'channel-searchbar',
   standalone: true,
-  imports: [IconLibaryComponent],
+  imports: [IconLibaryComponent, TranslocoModule],
   templateUrl: './channel-searchbar.component.html',
   styleUrl: './channel-searchbar.component.scss'
 })
