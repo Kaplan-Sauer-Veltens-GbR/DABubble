6fd88a9aceb5e7f03d09d7567fbb6161f27b0bd7
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-10-25 09:57:24 +0200

    implemented design draft for searchbar

--- src/app/main/header/channel-searchbar/channel-searchbar.component.ts
+++ src/app/main/header/channel-searchbar/channel-searchbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { IconLibaryComponent } from '../../../shared/components/icon-component/icon-libary.component';
 
 @Component({
   selector: 'channel-searchbar',
   standalone: true,
-  imports: [],
+  imports: [IconLibaryComponent],
   templateUrl: './channel-searchbar.component.html',
   styleUrl: './channel-searchbar.component.scss'
 })
