4e8bd94c5b6b565b256760111731632dd6f4f2be
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-10-25 11:50:37 +0200

    first draft of search-engine.service.ts. work on this later, when database is structured

--- src/app/services/search-engine.service.ts
+++ src/app/services/search-engine.service.ts
@@ -1,9 +1,36 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { collection, collectionData, Firestore, query, where } from '@angular/fire/firestore';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchEngineService {
 
-  constructor() { }
+  constructor() {}
+
+  firestore: Firestore = inject(Firestore);
+
+
+  //First implement Channel-Names search and go than further to all other functions
+  searchChannelNames(queryStr: string): Observable<any[]> {
+    const channelsRef = collection(this.firestore, 'channels');
+    const channelQuery = query(channelsRef, where('name', '>=', queryStr), where('name', '<=', queryStr + '\uf8ff'));
+    return collectionData(channelQuery);
+  }
+
+  searchPublicMessages(queryStr: string): Observable<any[]> {
+    const channelsRef = collection(this.firestore, 'channels'); //Proxy! Public Messages are part of channels
+    return collectionData(); 
+  }
+
+  searchPrivateMessages(queryStr: string): Observable<any[]> {
+    const privateMessagesRef = collection(this.firestore, 'privateMessages');
+    return collectionData();
+  }
+
+  searchUsers(queryStr: string): Observable<any[]> {
+    const usersRef = collection(this.firestore, 'users');
+    return collectionData();
+  }
 }
