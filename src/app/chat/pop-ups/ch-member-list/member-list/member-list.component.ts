54571d6f81338897850f556e73ad137d4362e881
Phillipcodes <Phillipsauer00@gmail.com>
2024-10-25 14:45:19 +0200

    added a global hover for user avatar and changed to the new component

--- src/app/chat/pop-ups/ch-member-list/member-list/member-list.component.ts
+++ src/app/chat/pop-ups/ch-member-list/member-list/member-list.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { AvatarBarComponent } from "../../../../shared/components/chat/avatar-bar/avatar-bar.component";
-import { UserAvatarTagComponent } from "../../../../shared/components/user-avatar-tag/user-avatar-tag.component";
 import { ReactionPopoverComponent } from "../../../../shared/components/chat/chat-message/reaction-popover/reaction-popover.component";
 import { IconLibaryComponent } from "../../../../shared/components/icon-component/icon-libary.component";
+import { UserAvatarComponent } from "../../../../shared/components/user-avatar/user-avatar.component";
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [AvatarBarComponent, UserAvatarTagComponent, ReactionPopoverComponent, IconLibaryComponent],
+  imports: [AvatarBarComponent, ReactionPopoverComponent, IconLibaryComponent, UserAvatarComponent],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.scss'
 })
