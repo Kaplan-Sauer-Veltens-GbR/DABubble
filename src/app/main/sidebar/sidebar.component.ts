bc066badfaf5764c4c7997640ec95fead1cd586c
Phillip <160394082+Phillipcodes@users.noreply.github.com>
2024-10-26 10:39:47 +0200

    add: user-avatar to sidebar
    placeholder loop for user-avater to introduce the style

--- src/app/main/sidebar/sidebar.component.ts
+++ src/app/main/sidebar/sidebar.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { IconLibaryComponent } from "../../shared/components/icon-component/icon-libary.component";
 import { CommonModule } from '@angular/common';
+import { AvatarBarComponent } from '../../shared/components/chat/avatar-bar/avatar-bar.component';
+import { UserAvatarComponent } from '../../shared/components/user-avatar/user-avatar.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [IconLibaryComponent,CommonModule],
+  imports: [IconLibaryComponent,CommonModule,UserAvatarComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
