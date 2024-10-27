dc6ae55db1c149c04c1058f18f7954ca6fa78f97
Phillipcodes <Phillipsauer00@gmail.com>
2024-10-27 15:54:01 +0100

    feat: function for freez the hover effect

--- src/app/services/workspace.service.ts
+++ src/app/services/workspace.service.ts
@@ -7,27 +7,34 @@ export class WorkspaceService {
   isOpen: boolean = true;
   currentDialog: 'channelEdit' | 'memberList' | 'addMember' | 'logOut' | null =
     null;
+  isSelected:boolean = false;
   constructor() {}
+
 
   toggle() {
     this.isOpen = !this.isOpen;
   }
 
+
   openDialog(dialog: 'channelEdit' | 'memberList' | 'addMember' | 'logOut') {
-    if(this.currentDialog === dialog) {
+    if (this.currentDialog === dialog) {
       console.log('close');
-      
+
       this.closeDialog();
-    }else {
+    } else {
       this.currentDialog = dialog;
       console.log(this.currentDialog, 'open');
     }
-
-    
-  
   }
+
 
   closeDialog() {
     this.currentDialog = null;
   }
+
+
+  whenSelected() {
+    this.isSelected = !this.isSelected;
+    }
 }
+
