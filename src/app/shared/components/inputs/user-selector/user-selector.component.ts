cab1230c5fbc8ae95b18a8a981ee93ebbfab07fa
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-10-27 08:13:49 +0100

    restructered code for better readability
    added servale helper-functions to clean the code

--- src/app/shared/components/inputs/user-selector/user-selector.component.ts
+++ src/app/shared/components/inputs/user-selector/user-selector.component.ts
@@ -4,6 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { UserAvatarComponent } from '../../user-avatar/user-avatar.component';
 import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
 
+interface ExampleUser {
+  name: string;
+  avatar: string;
+  timestamp?: number; // Optionales Feld, da es nur an bestimmten Stellen verwendet wird
+}
+
 @Component({
   selector: 'user-selector',
   standalone: true,
@@ -11,20 +17,22 @@ import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
   templateUrl: './user-selector.component.html',
   styleUrl: './user-selector.component.scss'
 })
+
 export class UserSelectorComponent {
+
   translocoService: TranslocoService = inject(TranslocoService);
 
   @ViewChild('search') searchInput!: ElementRef;
 
   searchText: string = '';
-  selectedUsers: { name: string, avatar: string, timestamp?: number }[] = [];
-  allUsers: { name: string, avatar: string }[] = [
+  selectedUsers: ExampleUser[] = [];
+  allUsers: ExampleUser[] = [
     { name: 'Elias Neumann', avatar: 'url' },
     { name: 'Antonia Neumann', avatar: 'url' },
     { name: 'Franziska Walther', avatar: 'url' },
     { name: 'Simone Münster', avatar: 'url' },
   ];
-  filteredUsers: { name: string, avatar: string }[] = this.allUsers;
+  filteredUsers: ExampleUser[] = this.allUsers;
   focusedChip: number = -1;
   focusedSuggestion: number = -1;
 
@@ -49,58 +57,71 @@ export class UserSelectorComponent {
   }
 
   // CAVE: Change this to User ID later, since users can have the same name
-  addUser(user: { name: string, avatar: string }) {
+  addUser(user: ExampleUser) {
     if (!this.selectedUsers.some(u => u.name === user.name)) {
       this.selectedUsers.push({ ...user, timestamp: this.returnTimestamp() });
     }
     this.searchText = '';
   }
 
-  removeUser(user: { name: string, avatar: string }) {
+  deleteUserInput() {
+    if (this.focusedChip !== -1) {
+      this.unselectUser(this.selectedUsers[this.focusedChip]);
+    } else {
+      this.unselectUser(this.selectedUsers[this.selectedUsers.length - 1]);
+    }
+    this.focusInput();
+  }
+
+  unselectUser(user: ExampleUser) {
     this.selectedUsers = this.selectedUsers.filter(u => u !== user);
-    console.log(this.selectedUsers);
-    
+  }
+
+  focusPreviousChip() {
+    if (this.isInitialChipSelection()) {
+      this.focusedChip = this.selectedUsers.length;
+    }
+    this.focusedChip--;
+    if (this.firstChipIsAllreadySelected()) {
+      this.focusedChip = 0;
+    }
+  }
+
+  isInitialChipSelection() {
+    return this.focusedChip === -1;
+  }
+
+  firstChipIsAllreadySelected() {
+    return this.focusedChip <= -1;
+  }
+
+  focusNextChip() {
+    this.nextChipExists() ? this.focusedChip++ : (this.focusedChip = -1);
+  }
+
+  nextChipExists() {
+    return this.focusedChip !== -1 && this.focusedChip < this.selectedUsers.length - 1;
+  }
+
+  resetChipFocus() {
+    this.focusedChip = -1;
   }
 
   handleKeyInput(event: KeyboardEvent) {
     if (!this.searchText && this.selectedUsers.length) {
       if (event.key === 'Backspace') {
-        if (this.focusedChip !== -1) {
-          this.removeUser(this.selectedUsers[this.focusedChip]);
-        } else {
-          this.removeUser(this.selectedUsers[this.selectedUsers.length - 1]);
-        } 
-        this.focusInput();
+        this.deleteUserInput();
       } else if (event.key === 'ArrowLeft') {
-        if (this.focusedChip === -1) {
-          this.focusedChip = this.selectedUsers.length;
-        }
-        this.focusedChip--;
-        if (this.focusedChip <= -1) {
-          this.focusedChip = 0;
-        }
-        console.log(event.key, this.focusedChip);
+        this.focusPreviousChip();
       } else if (event.key === 'ArrowRight') {
-        if (this.focusedChip !== -1 && this.focusedChip < this.selectedUsers.length -1) {
-          this.focusedChip++;
-        } else {
-          this.focusedChip = -1;
-        }
-        console.log(event.key, this.focusedChip);
+        this.focusNextChip();
       }
     }
-    // Maybe add logic for arrow-up/down as analogon for tab
-    // else if (this.searchText && this.filteredUsers.length) {
-    //   if (event.key === 'ArrowUp') {
-    //   } else if (event.key === 'ArrowDown') {
-    //   }
-    // }
   }
 
   focusInput() {
     this.searchInput.nativeElement.focus();
-    this.focusedChip = -1;
-    console.log(this.focusedChip);
-    
+    this.resetChipFocus();
   }
+
 }
