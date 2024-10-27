08da24fdeab7a31b7b537684be7ad7c1350f0b16
Phillipcodes <Phillipsauer00@gmail.com>
2024-10-27 13:05:39 +0100

    add: Html div seperator
    styling: seperator , day-tag
    add: textarea , text-messages

--- src/app/main/chat-window/chat-window.component.ts
+++ src/app/main/chat-window/chat-window.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { IconLibaryComponent } from "../../shared/components/icon-component/icon-libary.component";
 import { CommonModule } from '@angular/common';
 import { AvatarBarComponent } from "../../shared/components/chat/avatar-bar/avatar-bar.component";
+import { ChatMessageComponent } from '../../shared/components/chat/chat-message/chat-message.component';
+import { TextMessageFieldComponent } from "../../shared/components/inputs/text-message-field/text-message-field.component";
 
 @Component({
   selector: 'chat-window',
   standalone: true,
-  imports: [CommonModule, IconLibaryComponent, AvatarBarComponent],
+  imports: [CommonModule, IconLibaryComponent, AvatarBarComponent, ChatMessageComponent, TextMessageFieldComponent],
   templateUrl: './chat-window.component.html',
   styleUrl: './chat-window.component.scss'
 })
