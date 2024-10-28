145800b8baf79ce61dde60cc6f999f6e6379f993
Phillipcodes <Phillipsauer00@gmail.com>
2024-10-28 16:38:03 +0100

    feature: sidebar open and close animation

--- src/app/main/sidebar/sidebar.component.ts
+++ src/app/main/sidebar/sidebar.component.ts
@@ -1,17 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { IconLibaryComponent } from "../../shared/components/icon-component/icon-libary.component";
 import { CommonModule } from '@angular/common';
 import { AvatarBarComponent } from '../../shared/components/chat/avatar-bar/avatar-bar.component';
 import { UserAvatarComponent } from '../../shared/components/user-avatar/user-avatar.component';
+import { WorkspaceFabComponent } from "./workspace-fab/workspace-fab.component";
+import { WorkspaceService } from '../../services/workspace.service';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [IconLibaryComponent,CommonModule,UserAvatarComponent],
+  imports: [IconLibaryComponent, CommonModule, UserAvatarComponent, WorkspaceFabComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  public workspace = inject(WorkspaceService)
 @Input() selected:boolean = false;
 toggleChannel:boolean [] = [true,true];
 
