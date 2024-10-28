03361b97142133a7c3549f95f3581b9f8757aaf5
Phillipcodes <Phillipsauer00@gmail.com>
2024-10-28 14:59:25 +0100

    feat: merge first components togehter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChatWindowComponent } from "./main/chat-window/chat-window.component";
+import { HeaderComponent } from "./main/header/header.component";
+import { SidebarComponent } from "./main/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChatWindowComponent],
+  imports: [RouterOutlet, ChatWindowComponent, HeaderComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
