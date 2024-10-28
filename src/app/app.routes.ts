043c739adb4e2d3761712c1d4367115f5fedaac6
Phillipcodes <Phillipsauer00@gmail.com>
2024-10-28 19:32:28 +0100

    feat: enter submit for form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { PlaygroundComponent } from './playground/playground.component';
 import { ChatWindowComponent } from './main/chat-window/chat-window.component';
 
 
-
 export const routes: Routes = [
     {path: 'playground', component: PlaygroundComponent},
     {path: '', component: ChatWindowComponent},
