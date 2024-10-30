0c66a8e301d347bc01351b4c8d372d7ccc8cbebf
Phillip <160394082+Phillipcodes@users.noreply.github.com>
2024-10-30 11:27:06 +0100

    feat: input-field component,
    add: more transloco translations
    style: styled the column view

--- src/app/chat/pop-ups/create-channel/create-channel.component.ts
+++ src/app/chat/pop-ups/create-channel/create-channel.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { TranslocoModule } from '@jsverse/transloco';
 import { HeaderComponent } from "../../../main/header/header.component";
 import { IconLibaryComponent } from '../../../shared/components/icon-component/icon-libary.component';
+import { InputFieldsComponent } from "../../../shared/components/inputs/input-fields/input-fields.component";
 
 @Component({
   selector: 'app-create-channel',
   standalone: true,
-  imports: [CommonModule, TranslocoModule, HeaderComponent,IconLibaryComponent],
+  imports: [CommonModule, TranslocoModule, HeaderComponent, IconLibaryComponent, InputFieldsComponent],
   templateUrl: './create-channel.component.html',
   styleUrl: './create-channel.component.scss'
 })
