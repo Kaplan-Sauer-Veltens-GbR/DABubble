7c1540e8bb020f38c26e82ac5807391775f7d981
Phillip <160394082+Phillipcodes@users.noreply.github.com>
2024-10-30 10:45:02 +0100

    feat: add icon libary to component
    style: header styling

--- src/app/chat/pop-ups/create-channel/create-channel.component.ts
+++ src/app/chat/pop-ups/create-channel/create-channel.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslocoModule } from '@jsverse/transloco';
 import { HeaderComponent } from "../../../main/header/header.component";
+import { IconLibaryComponent } from '../../../shared/components/icon-component/icon-libary.component';
 
 @Component({
   selector: 'app-create-channel',
   standalone: true,
-  imports: [CommonModule, TranslocoModule, HeaderComponent],
+  imports: [CommonModule, TranslocoModule, HeaderComponent,IconLibaryComponent],
   templateUrl: './create-channel.component.html',
   styleUrl: './create-channel.component.scss'
 })
