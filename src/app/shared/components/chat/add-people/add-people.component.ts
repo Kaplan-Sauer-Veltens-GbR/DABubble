ac8b784742812cffd20801fd31e464cd90de4eec
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-11-01 17:06:50 +0100

    Added basic layout and rough functions  to add-people-component Needs further adjustments -> emitter for selected.users.length

--- src/app/shared/components/chat/add-people/add-people.component.ts
+++ src/app/shared/components/chat/add-people/add-people.component.ts
@@ -3,14 +3,43 @@ import { Component, Input } from '@angular/core';
 import { IconLibaryComponent } from "../../icon-component/icon-libary.component";
 import { TranslocoModule } from '@jsverse/transloco';
 import { UserSelectorComponent } from "../../inputs/user-selector/user-selector.component";
+import { ButtonComponent } from "../../inputs/button/button.component";
+import { RadioButtonComponent } from "../../inputs/radio-button/radio-button.component";
 
 @Component({
   selector: 'add-people-overlay',
   standalone: true,
-  imports: [CommonModule, IconLibaryComponent, TranslocoModule, UserSelectorComponent],
+  imports: [CommonModule, IconLibaryComponent, TranslocoModule, UserSelectorComponent, ButtonComponent, RadioButtonComponent],
   templateUrl: './add-people.component.html',
   styleUrl: './add-people.component.scss'
 })
 export class AddPeopleComponent {
-  @Input() curvedEdge: boolean = false;
+  @Input() standalone: boolean = true;
+
+  selectedUsers = [];
+
+  isFirstOption: boolean = true;
+
+  getSubmitButtonText(): string {
+    return !this.standalone ? 'add-button' : 'create-button';
+  }
+
+  toggleRadioButton(option: 0 | 1): boolean {
+    switch (option) {
+      case 0:
+        return this.isFirstOption = true;
+      case 1:
+        return this.isFirstOption = false;
+    }
+  }
+
+  isValid(): boolean {
+    if (this.standalone && this.isFirstOption) {
+      return true;
+    } else if (this.selectedUsers.length){
+      return true;
+    } else {
+      return false
+    }
+  }
 }
