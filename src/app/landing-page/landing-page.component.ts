11248e3077fee65425e0537038d32d7886461083
OkanKpl <okan149@gmail.com>
2024-11-03 16:37:04 +0100

    styling the reset.password component

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -3,11 +3,12 @@ import { SignInComponent } from "./sign-in/sign-in.component";
 import { SignUpComponent } from "./sign-up/sign-up.component";
 import { AvatarPickerComponent } from "./avatar-picker/avatar-picker.component";
 import { ResetPasswordEmailComponent } from "./reset-password-email/reset-password-email.component";
+import { ResetPasswordComponent } from "./reset-password/reset-password.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [SignInComponent, SignUpComponent, AvatarPickerComponent, ResetPasswordEmailComponent],
+  imports: [SignInComponent, SignUpComponent, AvatarPickerComponent, ResetPasswordEmailComponent, ResetPasswordComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
--- src/app/landing-page/reset-password/reset-password.component.ts
+++ src/app/landing-page/reset-password/reset-password.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { IconLibaryComponent } from "../../shared/components/icon-component/icon-libary.component";
+import { ButtonComponent } from "../../shared/components/inputs/button/button.component";
+import { InputFieldsComponent } from "../../shared/components/inputs/input-fields/input-fields.component";
 
 @Component({
   selector: 'app-reset-password',
   standalone: true,
-  imports: [],
+  imports: [IconLibaryComponent, ButtonComponent, InputFieldsComponent],
   templateUrl: './reset-password.component.html',
   styleUrl: './reset-password.component.scss'
 })
