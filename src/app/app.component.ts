c2fe3a8a1892944d0c9f57485896d01db536d2e0
OkanKpl <okan149@gmail.com>
2024-11-03 16:18:47 +0100

    adding icon slot to app-icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChatWindowComponent } from "./main/chat-window/chat-window.component";
+import { PlaygroundComponent } from "./playground/playground.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChatWindowComponent],
+  imports: [RouterOutlet, ChatWindowComponent, PlaygroundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
