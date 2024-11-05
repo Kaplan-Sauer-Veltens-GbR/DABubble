cc715513988d56573087b62f681b4fa4bbb48f12
Phillipcodes <Phillipsauer00@gmail.com>
2024-11-05 16:35:33 +0100

    delete

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { PlaygroundComponent } from './playground/playground.component';
 import { ChatWindowComponent } from './main/chat-window/chat-window.component';
+import { SignInComponent } from './landing-page/sign-in/sign-in.component';
 
 
 export const routes: Routes = [
