481605d13d4a5eb0f2ab6a9ba605fe390f4468e3
Phillipcodes <Phillipsauer00@gmail.com>
2024-11-05 17:12:51 +0100

    switched auth from own service to sigin.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { PlaygroundComponent } from './playground/playground.component';
 import { ChatWindowComponent } from './main/chat-window/chat-window.component';
 import { SignInComponent } from './landing-page/sign-in/sign-in.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
 export const routes: Routes = [
     {path: 'playground', component: PlaygroundComponent},
-    {path: '', component: ChatWindowComponent},
+    {path: '', component: LandingPageComponent},
 
 ];
--- src/app/landing-page/sign-in/sign-in.component.ts
+++ src/app/landing-page/sign-in/sign-in.component.ts
@@ -4,7 +4,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ButtonComponent } from '../../shared/components/inputs/button/button.component';
 import { InputFieldsComponent } from '../../shared/components/inputs/input-fields/input-fields.component';
 import { IconLibaryComponent } from '../../shared/components/icon-component/icon-libary.component';
-
+import {
+  Auth,
+  signInWithRedirect,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from '@angular/fire/auth';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-sign-in',
@@ -17,8 +23,30 @@ import { IconLibaryComponent } from '../../shared/components/icon-component/icon
     ReactiveFormsModule,
     ButtonComponent,
     InputFieldsComponent,
-    IconLibaryComponent
-
-  ]
+    IconLibaryComponent,
+  ],
 })
-export class SignInComponent { }
+export class SignInComponent {
+  private auth = inject(Auth);
+  private provider = new GoogleAuthProvider();
+
+  constructor() {}
+
+  signInWithGoogleRedirect() {
+    return signInWithRedirect(this.auth, this.provider);
+  }
+
+  signInWithGooglePopup() {
+    signInWithPopup(this.auth, this.provider).then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      // const token = credential?.accessToken;
+      // console.log(token);
+
+      // The signed-in user info.
+
+      const user = result.user;
+      console.log(user, credential);
+    });
+  }
+}
