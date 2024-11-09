0ba08a2708d1039ba0ed7a3e9bcc83a0475849ca
Phillipcodes <Phillipsauer00@gmail.com>
2024-11-09 17:45:46 +0100

    deleted unnecessary imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { ChatWindowComponent } from './main/chat-window/chat-window.component';
 import { SignInComponent } from './landing-page/sign-in/sign-in.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { MainComponent } from './main/main.component';
-import { AuthGuard } from '@angular/fire/auth-guard';
 
 export const routes: Routes = [
   { path: 'playground', component: PlaygroundComponent },
@@ -12,7 +11,6 @@ export const routes: Routes = [
   {
     path: 'main',
     component: MainComponent,
-    canActivate: [AuthGuard],
     children: [
       {
         path: 'user/:uid',
