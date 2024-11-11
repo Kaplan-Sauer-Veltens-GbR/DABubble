fb3c890680e7136c084ca1450439034d069e5e13
Phillip <160394082+Phillipcodes@users.noreply.github.com>
2024-11-11 11:22:53 +0100

    feat form validation vor email input

--- src/app/services/input-validation.service.ts
+++ src/app/services/input-validation.service.ts
@@ -1,51 +1,50 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InputValidationService {
-
-  constructor() { }
+  constructor() {}
 
   email: string = '';
   password: string = '';
   name: string = '';
-  profilePicture:string = '';
-  passedValidation:boolean = false
+  profilePicture: string = '';
+  passedValidation: boolean = false;
   onEmailChange(newEmail: string) {
-    this.email = newEmail;
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (emailPattern.test(newEmail)) {
+      this.email = newEmail;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   onPasswordChange(newPassword: string) {
     this.password = newPassword;
-    
   }
 
-  onNameChange(newName:string) {
-    debugger
+  onNameChange(newName: string) {
+    debugger;
     const namePattern = /^[A-Za-zÄÖÜäöüß]+([ '-][A-Za-zÄÖÜäöüß]+)*$/;
-    if(namePattern.test(newName) && this.name.length >= 3) {
+    if (namePattern.test(newName) && this.name.length >= 3) {
       this.name = newName;
-    return true;
-    }else {
-    
-     return false;
+      return true;
+    } else {
+      return false;
     }
   }
 
-  
-
-  onProfileChange(newProfilePicture:string) {
+  onProfileChange(newProfilePicture: string) {
     this.profilePicture = newProfilePicture;
   }
 
-
   checkIsFormValid() {
-    if(!this.passedValidation) {
+    if (!this.passedValidation) {
       return true;
-    }else {
+    } else {
       return false;
     }
   }
 }
- 
