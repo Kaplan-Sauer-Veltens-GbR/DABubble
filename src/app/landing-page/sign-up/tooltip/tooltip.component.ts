4990da91e037eff11591e3628c299b7209d920a8
Phillip <160394082+Phillipcodes@users.noreply.github.com>
2024-11-16 11:26:24 +0100

    added textet ffor the error message later transloco
    also added tool tip component for extra guideance

---
+++ src/app/landing-page/sign-up/tooltip/tooltip.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tooltip',
+  standalone: true,
+  imports: [],
+  templateUrl: './tooltip.component.html',
+  styleUrl: './tooltip.component.scss'
+})
+export class TooltipComponent {
+
+}
