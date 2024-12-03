c7e54da14a0c8ad7f07a09339132b38fa92cd3a4
Phillipcodes <Phillipsauer00@gmail.com>
2024-12-03 14:25:07 +0100

    injected imports for the reset  page

--- src/app/landing-page/reset-password-email/reset-password-email.component.ts
+++ src/app/landing-page/reset-password-email/reset-password-email.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ButtonComponent } from '../../shared/components/inputs/button/button.component';
 import { InputFieldsComponent } from '../../shared/components/inputs/input-fields/input-fields.component';
 import { IconLibaryComponent } from '../../shared/components/icon-component/icon-libary.component';
 import { RouterModule } from '@angular/router';
+import { InputValidationService } from '../../services/input-validation.service';
 
 @Component({
   selector: 'app-reset-password-email',
@@ -21,4 +22,6 @@ import { RouterModule } from '@angular/router';
   templateUrl: './reset-password-email.component.html',
   styleUrls: ['./reset-password-email.component.scss'],
 })
-export class ResetPasswordEmailComponent {}
+export class ResetPasswordEmailComponent {
+  public inputCheck = inject(InputValidationService)
+}
