67584ee199b0302d6aed9f996c2841cca0c7d6fc
Phillipcodes <Phillipsauer00@gmail.com>
2024-12-04 19:07:34 +0100

    add function for change password
    function for change the password on our site

--- src/app/landing-page/reset-password/reset-password.component.ts
+++ src/app/landing-page/reset-password/reset-password.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IconLibaryComponent } from "../../shared/components/icon-component/icon-libary.component";
 import { ButtonComponent } from "../../shared/components/inputs/button/button.component";
 import { InputFieldsComponent } from "../../shared/components/inputs/input-fields/input-fields.component";
 import { RouterLink } from '@angular/router';
+import { Auth, confirmPasswordReset } from '@angular/fire/auth';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -13,4 +15,19 @@ import { RouterLink } from '@angular/router';
 })
 export class ResetPasswordComponent {
 
+private auth = inject(Auth)
+
+ngOnInit(): void {
+  
+  
+}
+
+  async resetPassword(oobCode: string, newPassword:string) {
+    try {
+      await confirmPasswordReset(this.auth,oobCode,newPassword);
+    }catch(error) {
+      console.error('change of password went wrong' ,error);
+      
+    }
+  }
 }
