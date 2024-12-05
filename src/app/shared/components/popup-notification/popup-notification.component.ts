5576b4ab855b4709d4f1de9b978aff278594eb2e
Okan Kaplan <okan149@gmail.com>
2024-12-05 16:02:10 +0100

    adding and styling popup notification

---
+++ src/app/shared/components/popup-notification/popup-notification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-popup-notification',
+  standalone: true,
+  imports: [],
+  templateUrl: './popup-notification.component.html',
+  styleUrl: './popup-notification.component.scss'
+})
+export class PopupNotificationComponent {
+
+}
