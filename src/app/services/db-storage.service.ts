bbe3bda555e27ecf8a2968d0c046d2f50861488d
Phillipcodes <Phillipsauer00@gmail.com>
2024-12-12 12:49:00 +0100

    deleted firebaseconfig we already have it in the envoirment

--- src/app/services/db-storage.service.ts
+++ src/app/services/db-storage.service.ts
@@ -2,26 +2,19 @@ import { inject, Injectable } from '@angular/core';
 import { FirebaseApp, getApps, initializeApp } from '@angular/fire/app';
 import { FirbaseStorageConfig } from '../interfaces/firbase-storage-config';
 import { FirebaseStorage, getStorage } from '@angular/fire/storage';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DbStorageService {
 
-firebaseConfig:FirbaseStorageConfig = {
-    apiKey: '' ,
-    authDomain:'' ,
-    projectId: 'dabubble-530c4',
-    storageBucket: 'dabubble-530c4.appspot.com',
-    messagingSenderId: '',
-    appId: '' ,
 
-}
 private app: FirebaseApp;
 public storage: FirebaseStorage;
   constructor() { 
      if (!getApps().length) {
-      this.app = initializeApp(this.firebaseConfig);
+      this.app = initializeApp(environment.firebaseConfig);
     } else {
       this.app = getApps()[0]; 
     }
