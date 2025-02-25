81a43df3574e6d4c7bf26ac074736d8ce0fcafc8
OkanKpl <okan149@gmail.com>
2025-02-25 20:00:27 +0100

    adding responsive desgin to chat window

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -26,7 +26,7 @@ export class MainComponent implements OnInit {
   isAuthChecked = false;
 
 
-  isSmallScreen: boolean = window.innerWidth <= 760;
+  isSmallScreen: boolean = window.innerWidth <= 768;
   showChatWindow: boolean = false;
 
   ngOnInit(): void {
@@ -58,7 +58,7 @@ export class MainComponent implements OnInit {
   }
 
   private checkScreenSize(): void {
-    this.isSmallScreen = window.innerWidth <= 760;
+    this.isSmallScreen = window.innerWidth <= 768;
     if (!this.isSmallScreen) {
 
       this.showChatWindow = false;
