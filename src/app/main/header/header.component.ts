8fbf7193a3fa9d1065ed43d2aa2170676c7a030f
OkanKpl <okan149@gmail.com>
2025-03-12 17:48:40 +0100

    responsive design for header

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,15 +1,23 @@
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, inject, Input,Output } from '@angular/core';
 import { ChannelSearchbarComponent } from './channel-searchbar/channel-searchbar.component';
 import { AvatarMenuComponent } from './avatar-menu/avatar-menu.component';
+import { NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ChannelSearchbarComponent, AvatarMenuComponent],
+  imports: [ChannelSearchbarComponent, AvatarMenuComponent,NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  @Input() isSmallScreen: boolean = false;
+  @Input() showChatWindow: boolean = false;
 
+  @Output() closeChatEvent = new EventEmitter<void>();
+
+  closeChat() {
+    this.closeChatEvent.emit();
+  }
 }
